1400e6ea0505bb9d57a5d76de0e678e7da4bd9a6
abdalrzag eisa <bmjfdrh@gmail.com>
2023-07-13 23:25:14 +0300

    models

--- server/src/models.rs
+++ server/src/models.rs
@@ -1,7 +1,7 @@
-use crate::schema::shows;
+use crate::schema::{openings, shows};
 use uuid::Uuid;
 
-#[derive(diesel_derive_enum::DbEnum, std::fmt::Debug)]
+#[derive(diesel_derive_enum::DbEnum, std::fmt::Debug, PartialEq)]
 #[ExistingTypePath = "crate::schema::sql_types::ShowFormat"]
 #[DbValueStyle = "UPPERCASE"]
 pub enum ShowFormat {
@@ -12,7 +12,7 @@ pub enum ShowFormat {
     SPECIAL,
 }
 
-#[derive(diesel_derive_enum::DbEnum, std::fmt::Debug)]
+#[derive(diesel_derive_enum::DbEnum, std::fmt::Debug, PartialEq)]
 #[ExistingTypePath = "crate::schema::sql_types::ShowStatus"]
 #[DbValueStyle = "UPPERCASE"]
 pub enum ShowStatus {
@@ -20,7 +20,7 @@ pub enum ShowStatus {
     ONGOING,
 }
 
-#[derive(diesel_derive_enum::DbEnum, std::fmt::Debug)]
+#[derive(diesel_derive_enum::DbEnum, std::fmt::Debug, PartialEq)]
 #[ExistingTypePath = "crate::schema::sql_types::ShowSeason"]
 #[DbValueStyle = "UPPERCASE"]
 pub enum ShowSeason {
@@ -45,7 +45,7 @@ pub struct NewShow {
     pub banner: Vec<u8>,
 }
 
-#[derive(Debug, Queryable, AsChangeset)]
+#[derive(Debug, Queryable, AsChangeset, Identifiable, Selectable, PartialEq)]
 pub struct Show {
     pub id: Uuid,
     pub title: String,
@@ -57,4 +57,24 @@ pub struct Show {
     pub folder_name: String,
     pub image: Vec<u8>,
     pub banner: Vec<u8>,
+}
+
+#[derive(Insertable)]
+#[diesel(table_name = openings)]
+pub struct NewOpening {
+    pub id: Uuid,
+    pub show_id: Uuid,
+    pub title: String,
+    pub file_name: String,
+    pub thumbnail: Vec<u8>,
+}
+
+#[derive(Queryable, Selectable, Identifiable, Associations, Debug, PartialEq)]
+#[diesel(belongs_to(Show))]
+pub struct Opening {
+    pub id: Uuid,
+    pub show_id: Uuid,
+    pub title: String,
+    pub file_name: String,
+    pub thumbnail: Vec<u8>,
 }
