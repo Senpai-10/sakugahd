3eee38fa501f0009e0655b304154d32aeb27a4a9
abdalrzag eisa <bmjfdrh@gmail.com>
2023-07-20 06:24:02 +0300

    clippy

--- server/src/routes/video.rs
+++ server/src/routes/video.rs
@@ -15,24 +15,21 @@ pub fn video_episodes<'a>(title: String, ep_number: i32) -> std::io::Result<Seek
     let abs_path = std::path::Path::new(&anime_directory);
     let mut file_path = String::new();
 
-    match schema::episodes::dsl::episodes
+    if let Ok(v) = schema::episodes::dsl::episodes
         .filter(schema::episodes::show_title.eq(&title))
         .filter(schema::episodes::number.eq(&ep_number))
         .first::<Episode>(&mut conn)
     {
-        Ok(v) => {
-            let file_name: String = v.file_name;
+        let file_name: String = v.file_name;
 
-            file_path = String::from(
-                abs_path
-                    .join(title)
-                    .join("episodes")
-                    .join(file_name)
-                    .to_str()
-                    .unwrap(),
-            )
-        }
-        Err(_) => {}
+        file_path = String::from(
+            abs_path
+                .join(title)
+                .join("episodes")
+                .join(file_name)
+                .to_str()
+                .unwrap(),
+        )
     };
 
     SeekStream::from_path(file_path)
@@ -45,24 +42,21 @@ pub fn video_movies<'a>(title: String, ep_number: i32) -> std::io::Result<SeekSt
     let abs_path = std::path::Path::new(&anime_directory);
     let mut file_path = String::new();
 
-    match schema::movies::dsl::movies
+    if let Ok(v) = schema::movies::dsl::movies
         .filter(schema::movies::show_title.eq(&title))
         .filter(schema::movies::number.eq(&ep_number))
         .first::<Movie>(&mut conn)
     {
-        Ok(v) => {
-            let file_name: String = v.file_name;
+        let file_name: String = v.file_name;
 
-            file_path = String::from(
-                abs_path
-                    .join(title)
-                    .join("movies")
-                    .join(file_name)
-                    .to_str()
-                    .unwrap(),
-            )
-        }
-        Err(_) => {}
+        file_path = String::from(
+            abs_path
+                .join(title)
+                .join("movies")
+                .join(file_name)
+                .to_str()
+                .unwrap(),
+        )
     };
 
     SeekStream::from_path(file_path)
@@ -75,24 +69,21 @@ pub fn video_openings<'a>(title: String, ep_number: i32) -> std::io::Result<Seek
     let abs_path = std::path::Path::new(&anime_directory);
     let mut file_path = String::new();
 
-    match schema::openings::dsl::openings
+    if let Ok(v) = schema::openings::dsl::openings
         .filter(schema::openings::show_title.eq(&title))
         .filter(schema::openings::number.eq(&ep_number))
         .first::<Opening>(&mut conn)
     {
-        Ok(v) => {
-            let file_name: String = v.file_name;
+        let file_name: String = v.file_name;
 
-            file_path = String::from(
-                abs_path
-                    .join(title)
-                    .join("openings")
-                    .join(file_name)
-                    .to_str()
-                    .unwrap(),
-            )
-        }
-        Err(_) => {}
+        file_path = String::from(
+            abs_path
+                .join(title)
+                .join("openings")
+                .join(file_name)
+                .to_str()
+                .unwrap(),
+        )
     };
 
     SeekStream::from_path(file_path)
@@ -105,24 +96,21 @@ pub fn video_endings<'a>(title: String, ep_number: i32) -> std::io::Result<SeekS
     let abs_path = std::path::Path::new(&anime_directory);
     let mut file_path = String::new();
 
-    match schema::endings::dsl::endings
+    if let Ok(v) = schema::endings::dsl::endings
         .filter(schema::endings::show_title.eq(&title))
         .filter(schema::endings::number.eq(&ep_number))
         .first::<Ending>(&mut conn)
     {
-        Ok(v) => {
-            let file_name: String = v.file_name;
+        let file_name: String = v.file_name;
 
-            file_path = String::from(
-                abs_path
-                    .join(title)
-                    .join("endings")
-                    .join(file_name)
-                    .to_str()
-                    .unwrap(),
-            )
-        }
-        Err(_) => {}
+        file_path = String::from(
+            abs_path
+                .join(title)
+                .join("endings")
+                .join(file_name)
+                .to_str()
+                .unwrap(),
+        )
     };
 
     SeekStream::from_path(file_path)
