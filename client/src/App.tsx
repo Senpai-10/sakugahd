2d8ab0d81fe642b287b27d4d029d9f74f7981390
abdalrzag eisa <bmjfdrh@gmail.com>
2023-07-21 15:10:00 +0300

    setup client in reactjs not yew

---
+++ client/src/App.tsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import axios from 'axios';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+    const [data, setData] = useState('');
+
+    function sendReq() {
+        axios
+            .get('/api/shows')
+            .then((res) => setData(JSON.stringify(res.data)));
+    }
+
+    return (
+        <>
+            <div>
+                <a href='https://vitejs.dev' target='_blank'>
+                    <img src={viteLogo} className='logo' alt='Vite logo' />
+                </a>
+                <a href='https://react.dev' target='_blank'>
+                    <img
+                        src={reactLogo}
+                        className='logo react'
+                        alt='React logo'
+                    />
+                </a>
+            </div>
+            <h1>Vite + React</h1>
+            <div className='card'>
+                <button onClick={sendReq}>Get data</button>
+                <p>
+                    data: {data}
+                </p>
+            </div>
+            <p className='read-the-docs'>
+                Click on the Vite and React logos to learn more
+            </p>
+        </>
+    );
+}
+
+export default App;
