3035b9e2106e00f2512d8dcd68c9005d0104b9e9
abdalrzag eisa <bmjfdrh@gmail.com>
2023-07-22 05:30:56 +0300

    fetch and display shows

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,42 +1,61 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
-function App() {
-    const [data, setData] = useState('');
+function bytesToBase64(bytes: number[] | undefined): string {
+    if (bytes == undefined) {
+        return '';
+    }
 
-    function sendReq() {
+    const binString = Array.from(bytes, (x) => String.fromCodePoint(x)).join(
+        ''
+    );
+    return btoa(binString);
+}
+
+type ShowFormat = 'TV' | 'OVA' | 'ONA' | 'MOVIE' | 'SPECIAL';
+type ShowSeason = 'SPRING' | 'SUMMER' | 'FALL' | 'WINTER';
+type ShowStatus = 'FINISHED' | 'ONGOING';
+
+interface Show {
+    title: string;
+    description: String;
+    format?: ShowFormat;
+    status?: ShowStatus;
+    season?: ShowSeason;
+    season_year?: number;
+    cover?: number[];
+}
+
+function App() {
+    const [shows, setShows] = useState<Show[]>([]);
+
+    useEffect(() => {
         axios
             .get('/api/shows')
-            .then((res) => setData(JSON.stringify(res.data)));
-    }
+            .then((res) => res.data)
+            .then((data) => setShows(data));
+    }, []);
 
     return (
         <>
-            <div>
-                <a href='https://vitejs.dev' target='_blank'>
-                    <img src={viteLogo} className='logo' alt='Vite logo' />
-                </a>
-                <a href='https://react.dev' target='_blank'>
-                    <img
-                        src={reactLogo}
-                        className='logo react'
-                        alt='React logo'
-                    />
-                </a>
+            <div className='shows'>
+                {shows.map((show) => (
+                    <div
+                        className='show'
+                        style={{
+                            backgroundImage: `url(data:image/png;base64,${bytesToBase64(
+                                show.cover
+                            )})`,
+                        }}
+                        key={show.title}
+                    >
+                        <div className='show-overlay'>
+                            <p className='show-title'>{show.title}</p>
+                        </div>
+                    </div>
+                ))}
             </div>
-            <h1>Vite + React</h1>
-            <div className='card'>
-                <button onClick={sendReq}>Get data</button>
-                <p>
-                    data: {data}
-                </p>
-            </div>
-            <p className='read-the-docs'>
-                Click on the Vite and React logos to learn more
-            </p>
         </>
     );
 }
