04ff32c7fea9780a163787557636a9dfe1f69e7f
abdalrzag eisa <bmjfdrh@gmail.com>
2023-07-25 09:15:21 +0300

    list genres

---
+++ client/src/pages/genres_page.tsx
@@ -0,0 +1,22 @@
+import {Link} from 'react-router-dom'
+import { useState, useEffect } from "react"
+import axios from "axios";
+
+export function Genres_page() {
+    const [genres, setGenres] = useState<string[]>([])
+
+    useEffect(() => {
+        axios.get(`/api/genres`)
+            .then((res) => res.data)
+            .then((data) => setGenres(data));
+    }, [])
+
+    return (
+        <>
+            {genres.map((genres_name) => (
+                <Link to={`/genres/${genres_name}`}><button>{genres_name}</button></Link>
+            ))}
+        </>
+    )
+}
+
